f10be7b0aaba8cef5aeccc351d6401da2b13a1d6
Abid-Noyon <abidnoyon49@gmail.com>
2022-09-17 11:20:44 +0600

    added ErrorsWarning and comments folder

---
+++ 0.Template/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    
+    //compile time error if you forget to put semicolon at the last
+    std::cout << "Hello World" << std::endl;
+    return 0;
+
+}
---
+++ 4.Comments/main.cpp
@@ -0,0 +1,18 @@
+// My First C++ Program
+
+//This brings in the iostream library
+#include <iostream>
+
+/*
+    This is a block comment that englobes multiple 
+    line of text
+    block comment can not be nested
+*/
+
+int main(){
+    
+    //This code will wite Hello World! in the console
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+    //program ends here
+}
