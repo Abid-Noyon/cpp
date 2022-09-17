9d071e783785d2ac5db4d421503be269caaf1d0b
Abid-Noyon <abidnoyon49@gmail.com>
2022-09-17 10:16:40 +0600

    updated

--- 3.FirstCppProgram/main.cpp
+++ 3.FirstCppProgram/main.cpp
@@ -1,8 +1,12 @@
-// Your First C++ Program
+// My First C++ Program
 
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!";
+int main(){
+
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2" << std::endl;
+    std::cout << "Number3" << std::endl;
+
     return 0;
 }
