8fcb9fdc101d6c3049b6d02d062b502a6eb30226
Abid-Noyon <abidnoyon49@gmail.com>
2022-09-19 12:01:38 +0600

    Number system added

--- 6.DataInputAndOutput/main.cpp
+++ 6.DataInputAndOutput/main.cpp
@@ -36,10 +36,11 @@ int main(){
 
     std::cout << "Please type your full name and age " << std::endl;
 
-    std::getline(std::cin,full_name);
+    std:getline(std::cin, full_name);
     std::cin >> age3;
 
-    std::cout << "Hello! " << full_name << " you are " << age3 << " years old! " << std::endl;
+    std::cout << "Hello! " << full_name << " you are " << age3 << " years old!" << std::endl;
+
 
     return 0;
 }
