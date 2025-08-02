06c83ea81636dee4067113c70d09ee9484fc8064
Md. Abid Hassan <md.abidhassan@Abids-MacBook-Air.local>
2025-08-02 17:24:07 +0600

    updated more cpp files

---
+++ learning/The-C-20-Masterclass/03.FirstSteps/3.6DataInputAndOutput/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+std::string full_name;
+  int age3;
+
+  std::cout << "Please type in your full name and age " << std::endl;
+
+  std::getline(std::cin, full_name);
+
+  std::cin >> age3;
+
+  std::cout << "Hello " << full_name
+            << " you are " << age3 << " years old!" << std::endl;
